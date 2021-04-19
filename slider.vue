5213568d51bcfd2c41d072c89d200874e9d8ed80
chocolate-dog <76462900+chocolate-dog@users.noreply.github.com>
2021-04-19 16:44:56 +0800

    slider
    123

---
+++ slider.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="er-slider">
+    <div class="slider">
+      <div
+        class="slider1" :style="{
+          width: (this.value / 100 * 600 )+ 'px'
+        }"
+      ></div>
+      <div
+        class="slider2" :style="{
+          left:((this.value / 100) * 600 > 22? 
+          (this.value / 100) * 600 - 7
+              : 0) + 'px'
+        }"
+      @click="press"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: Number
+  },
+  methods: {
+    press(e){
+      console.log(e)
+      // console.log(offsetLeft)
+      // console.log(e.pageX)
+      
+    },
+  }
+};
+</script>
+<style scoped>
+.slider {
+  margin: 100px auto;
+  width: 600px;
+  height: 8px;
+  background-color: white;
+  border-radius: 4px;
+  border: 1px solid rgb(78, 115, 175);
+}
+.slider1 {
+  width: 300px;
+  height: 8px;
+  background-color: rgb(78, 115, 175);
+  border-radius: 4px;
+}
+.slider2 {
+  height: 14px;
+  width: 14px;
+  border-radius: 50%;
+  border: 1px solid rgb(78, 115, 175);
+  background-color: white;
+  position: relative;
+  bottom: 12px;
+  left: 293px;
+}
+</style>
